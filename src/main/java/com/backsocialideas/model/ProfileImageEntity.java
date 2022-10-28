c1016e0c9a67e0c23c1d1daa2afefad0e65910c4
TazzSoni <tazzsoni@gmail.com>
2022-10-28 11:03:11 -0300

    Criaçã imagem perfil

---
+++ src/main/java/com/backsocialideas/model/ProfileImageEntity.java
@@ -0,0 +1,30 @@
+package com.backsocialideas.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "anexo")
+public class ProfileImageEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+
+    private String contentType;
+
+    private Long size;
+
+    @Lob
+    private byte[] data;
+}
