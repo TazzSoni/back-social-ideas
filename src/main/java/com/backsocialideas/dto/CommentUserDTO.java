7bf225ee88a3d395456736952e53a7e293b7a6c8
TazzSoni <tazzsoni@gmail.com>
2022-11-09 09:46:28 -0300

    delete requisição cooworker ao aceitar cooworker

--- src/main/java/com/backsocialideas/dto/CommentUserDTO.java
+++ src/main/java/com/backsocialideas/dto/CommentUserDTO.java
@@ -12,4 +12,5 @@ public class CommentUserDTO {
     private Long id;
     private String name;
     private int level;
+    private boolean teacher;
 }
--- src/main/java/com/backsocialideas/dto/PostUserDTO.java
+++ src/main/java/com/backsocialideas/dto/PostUserDTO.java
@@ -12,4 +12,5 @@ public class PostUserDTO {
     private Long id;
     private String name;
     private int level;
+    private boolean teacher;
 }
